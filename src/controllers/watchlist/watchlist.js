92217f74ed98c079915a62478d83567f96b09d3a
Juliana <jumiranda0501@gmail.com>
2021-11-16 12:56:45 -0300

    Removed function to add isBookmarked

--- src/controllers/watchlist/watchlist.js
+++ src/controllers/watchlist/watchlist.js
@@ -16,25 +16,10 @@ export const watchlist = async (req, res, next) => {
       //debug(`Movies: ${watchlistDocument.movies.length}`);
       //debug(`Series: ${watchlistDocument.series.length}`);
 
-      const movies = watchlistDocument.movies;
-      const series = watchlistDocument.series;
-
-      if (movies.length > 0) {
-        movies.forEach(movie => {
-          movie.isBookmarked = true;
-        });
-      }
-
-      if (series.length > 0) {
-        series.forEach(serie => {
-          serie.isBookmarked = true;
-        });
-      }
-
       return res.json({
         message: 'Watchlist document found',
-        series: series,
-        movies: movies
+        series: watchlistDocument.series,
+        movies: watchlistDocument.movies
       });
     }
     else {
